b4da7764232e875e88e2e735218f9844412553cc
Anil Madhavapeddy <anil@recoil.org>
2014-08-07 23:15:18 +0100

    Travis: reduce verbosity

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -13,7 +13,6 @@ sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam time libssl-dev
 
 export OPAMYES=1
-export OPAMVERBOSE=1
 echo OCaml version
 ocaml -version
 echo OPAM versions
@@ -37,11 +36,13 @@ make clean
 make
 make install
 
+export OPAMVERBOSE=1
 opam install async_ssl
 make clean
 make
 make install
 
+export OPAMVERBOSE=0
 opam install lwt
 make clean
 make

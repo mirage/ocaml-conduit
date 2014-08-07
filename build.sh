abd84af23b38ee7721a9764024d23dc306b674a3
Anil Madhavapeddy <anil@recoil.org>
2014-08-07 22:33:27 +0100

    Re-enable Async SSL by default

--- build.sh
+++ build.sh
@@ -19,9 +19,6 @@ case $OCAML_VERSION in
 *)
 HAVE_ASYNC=`ocamlfind query async 2>/dev/null || true`
 HAVE_ASYNC_SSL=`ocamlfind query async_ssl 2>/dev/null || true`
-echo Async SSL requires a stub-generation version due to
-echo a linking order bug. This release has it temporarily disabled.
-HAVE_ASYNC_SSL=
 ;;
 esac
 

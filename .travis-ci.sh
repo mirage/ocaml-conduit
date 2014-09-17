3dfa4cc91c630b8997a112e592f0e197bfac1a51
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-09-17 15:55:38 +0000

    Install libxen-dev and uuid-dev in travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -11,6 +11,7 @@ esac
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam time libssl-dev
+sudo apt-get install libxen-dev uuid-dev
 
 export OPAMYES=1
 echo OCaml version

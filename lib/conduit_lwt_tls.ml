83db584bf4f77820c3dc0a77559f2f7e11a78236
Thomas Leonard <talex5@gmail.com>
2015-02-02 16:55:46 +0000

    Update for new ocaml-tls API

--- lib/conduit_lwt_tls.ml
+++ lib/conduit_lwt_tls.ml
@@ -59,7 +59,7 @@ module Server = struct
   let init ?(nconn=20) ~certfile ~keyfile
       ?(stop = fst (Lwt.wait ())) ?timeout sa callback =
     X509_lwt.private_of_pems ~cert:certfile ~priv_key:keyfile >>= fun certificate ->
-    let config = Tls.Config.server ~certificate () in
+    let config = Tls.Config.server ~certificates:(`Single certificate) () in
     let s = listen nconn sa in
     let cont = ref true in
     async (fun () ->

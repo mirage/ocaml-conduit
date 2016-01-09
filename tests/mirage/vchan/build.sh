08d1883bfb1ea0496fbad25c0d6c8227b4ba1937
Jon Ludlam <jonathan.ludlam@citrix.com>
2016-01-09 02:28:08 +0000

    Fix vchan example code, and use functoria-style mirage for it
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

--- tests/mirage/vchan/build.sh
+++ tests/mirage/vchan/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
-mirage configure config_server.ml --xen --no-opam
+mirage configure -f config_server.ml --xen --no-opam
 make
-mirage configure config_client.ml --xen --no-opam
+mirage configure -f config_client.ml --xen --no-opam
 make

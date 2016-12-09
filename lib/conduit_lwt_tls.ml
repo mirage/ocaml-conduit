a15326e8b4cc3903fffbeacfb4283dc302d15820
Török Edwin <edwin@skylable.com>
2016-12-10 00:19:04 +0200

    lwt_tls: should also process the connection, not just accept it
    This was lost apparently in fd2086a377df18337559aada7992deb02ca976ef.
    ignore_result is error prone, next commit will fix that.
    
    You can test with:
    $ cd tests/unix && CONDUIT_TLS=native ./run_tls.sh

--- lib/conduit_lwt_tls.ml
+++ lib/conduit_lwt_tls.ml
@@ -58,6 +58,7 @@ module Server = struct
              let (ic, oc) = Tls_lwt.of_t t in
              Lwt.return (fd, ic, oc))
           (fun exn -> Lwt_unix.close fd >>= fun () -> Lwt.fail exn)
+        >|= Conduit_lwt_server.process_accept ?timeout callback
         |> Lwt.ignore_result)
 
   let init ?backlog ~certfile ~keyfile ?stop ?timeout sa callback =

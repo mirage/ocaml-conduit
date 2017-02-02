dfc0c31fd457564c8fae02d2ecbec10473763d6f
Vincent Bernardoff <vb@luminar.eu.org>
2017-02-02 17:19:27 +0100

    fix memory leak

--- lib/conduit_async_ssl.ml
+++ lib/conduit_async_ssl.ml
@@ -20,10 +20,9 @@ open Core.Std
 open Async.Std
 open Async_ssl.Std
 
-let ssl_connect ?version ?name ?ca_file ?ca_path ?session ?verify
-    net_to_ssl ssl_to_net =
-  let net_to_ssl = Reader.pipe net_to_ssl in
-  let ssl_to_net = Writer.pipe ssl_to_net in
+let ssl_connect ?version ?name ?ca_file ?ca_path ?session ?verify r w =
+  let net_to_ssl = Reader.pipe r in
+  let ssl_to_net = Writer.pipe w in
   let app_to_ssl, app_wr = Pipe.create () in
   let app_rd, ssl_to_app = Pipe.create () in
   let verify_connection = match verify with
@@ -45,15 +44,27 @@ let ssl_connect ?version ?name ?ca_file ?ca_path ?session ?verify
   >>= fun conn ->
   verify_connection conn >>= function
   | false ->
+    Ssl.Connection.close conn ;
+    Pipe.close_read app_rd ;
+    Writer.close w >>= fun () ->
     failwith "Connection verification failed."
   | true ->
-    Reader.of_pipe (Info.of_string "async_conduit_ssl_reader") app_rd >>= fun app_rd ->
-    Writer.of_pipe (Info.of_string "async_conduit_ssl_writer") app_wr >>| fun (app_wr,_) ->
-    (app_rd, app_wr)
+    Reader.of_pipe (Info.of_string "async_conduit_ssl_reader") app_rd >>= fun app_reader ->
+    Writer.of_pipe (Info.of_string "async_conduit_ssl_writer") app_wr >>| fun (app_writer,_) ->
+    don't_wait_for begin
+      Deferred.all_unit [
+        Writer.close_finished app_writer ;
+        Reader.close_finished app_reader ;
+      ] >>= fun () ->
+      Ssl.Connection.close conn ;
+      Pipe.close_read app_rd ;
+      Writer.close w ;
+    end ;
+    (app_reader, app_writer)
 
-let ssl_listen ?(version=Ssl.Version.Tlsv1_2) ?ca_file ?ca_path ~crt_file ~key_file rd wr =
-  let net_to_ssl = Reader.pipe rd in
-  let ssl_to_net = Writer.pipe wr in
+let ssl_listen ?(version=Ssl.Version.Tlsv1_2) ?ca_file ?ca_path ~crt_file ~key_file r w =
+  let net_to_ssl = Reader.pipe r in
+  let ssl_to_net = Writer.pipe w in
   let app_to_ssl, app_wr = Pipe.create () in
   let app_rd, ssl_to_app = Pipe.create () in
   Ssl.server
@@ -69,11 +80,15 @@ let ssl_listen ?(version=Ssl.Version.Tlsv1_2) ?ca_file ?ca_path ~crt_file ~key_f
     ()
   |> Deferred.Or_error.ok_exn
   >>= fun conn ->
-  Reader.of_pipe (Info.of_string "async_conduit_ssl_reader") app_rd >>= fun app_rd ->
-  Writer.of_pipe (Info.of_string "async_conduit_ssl_writer") app_wr >>| fun (app_wr,_) ->
+  Reader.of_pipe (Info.of_string "async_conduit_ssl_reader") app_rd >>= fun app_reader ->
+  Writer.of_pipe (Info.of_string "async_conduit_ssl_writer") app_wr >>| fun (app_writer,_) ->
   don't_wait_for begin
-    Deferred.all_unit [Reader.close_finished app_rd;
-                       Writer.close_finished app_wr] >>| fun () ->
-    Ssl.Connection.close conn
+    Deferred.all_unit [
+      Reader.close_finished app_reader;
+      Writer.close_finished app_writer
+    ] >>= fun () ->
+    Ssl.Connection.close conn ;
+    Pipe.close_read app_rd ;
+    Writer.close w ;
   end;
-  (app_rd, app_wr)
+  (app_reader, app_writer)

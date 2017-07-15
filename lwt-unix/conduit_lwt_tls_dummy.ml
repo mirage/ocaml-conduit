c46f6e91c175454018109a316b990b68e5514d1b
Rudi Grinberg <rudi.grinberg@gmail.com>
2017-07-15 03:30:22 -0400

    Make tls optional
    Just like SSL is optional

---
+++ lwt-unix/conduit_lwt_tls_dummy.ml
@@ -0,0 +1,12 @@
+module Client = struct
+  let connect ?src host sa =
+    Lwt.fail_with "Tls not available"
+end
+
+module Server = struct
+  let init' ?backlog ?stop ?timeout tls sa callback =
+    Lwt.fail_with "Tls not available"
+
+  let init ?backlog ~certfile ~keyfile ?stop ?timeout sa callback =
+    Lwt.fail_with "Tls not available"
+end

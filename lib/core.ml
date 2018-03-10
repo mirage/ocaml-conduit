1dcde6ad7c145a3f3e0b6ddedb1131f7a7b2ae2d
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-03-10 08:35:20 +0700

    Remove core.mli
    There's no need for an .mli file if there's only types in the module

--- lib/core.ml
+++ lib/core.ml
@@ -1,5 +1,5 @@
 (*
- * Copyright (c) 2012-2014 Anil Madhavapeddy <anil@recoil.org>
+ * Copyright (c) 2014 Anil Madhavapeddy <anil@recoil.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -15,19 +15,53 @@
  *
 *)
 
+(** Interface for establishing reliable stream-oriented connections.
+
+    This library abstracts the concerns of establishing connections to
+    peers that may be running within the same host (e.g. in another
+    virtual machine) or on a remote host via TCP.  It consists of one
+    library that is responsible for {{!transport}establishing individual
+    connections}, and a {{!resolution}name resolver} that maps URIs
+    to endpoints.
+
+    {2:transport Connection Establishment}
+
+    Connections are created by identifying remote nodes using an
+    {{!endp}endp} value.  To ensure portability, the {!endp} values
+    are translated into concrete connections by separate modules that
+    target [Lwt_unix], [Async] and [Mirage].  This lets those backends
+    use the appropriate local technique for creating the connection
+    (such as using OpenSSL on Unix, or a pure OCaml TLS+TCP
+    implementation on Mirage, or some other combination).
+
+    The modules dealing with connection establishment are:
+    {!modules: Conduit_lwt_unix Conduit_async Conduit_mirage}
+
+    {2:resolution Name Resolution}
+
+    This deals with resolving URIs into a list of {!endp} addresses that can
+    then be connected to by the {{!transport}connection establishment} modules.
+
+    All of the name resolvers conform to the {!RESOLVER} module type.
+    The OS-specific implementations of this interface are:
+    {!modules: Conduit_lwt.Resolver Resolver_lwt_unix Resolver_mirage}
+ *)
+
 open Sexplib.Std
 
-(** The resolver will return an [endp], which the Conduit
-    backend must interpret to make a connection. *)
+(** End points that can potentially be connected to.
+    These are typically returned by a call to a {{!resolution}resolver}. *)
 type endp = [
-  | `TCP of Ipaddr.t * int        (** ipaddr and dst port *)
-  | `Unix_domain_socket of string (** unix file path *)
-  | `Vchan_direct of int * string        (** domain id, port *)
-  | `Vchan_domain_socket of string * string
-  | `TLS of string * endp         (** wrap in a TLS channel, [hostname,endp] *)
-  | `Unknown of string            (** failed resolution *)
+  | `TCP of Ipaddr.t * int         (** IP address and destination port *)
+  | `Unix_domain_socket of string  (** Unix domain file path *)
+  | `Vchan_direct of int * string  (** domain id, port *)
+  | `Vchan_domain_socket of string * string (** Vchan Xen domain socket *)
+  | `TLS of string * endp          (** Wrap in a TLS channel, [hostname,endp] *)
+  | `Unknown of string             (** Failed resolution *)
 ] [@@deriving sexp]
 
+(** Module type for cooperative threading that can be satisfied by
+    Lwt or Async *)
 module type IO = sig
   type +'a t
   val (>>=) : 'a t -> ('a -> 'b t) -> 'b t

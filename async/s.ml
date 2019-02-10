0ba9fc08cdccb794c34434f5d4d1b0367f5f38c2
Vincent Bernardoff <vb@luminar.eu.org>
2019-02-10 15:41:21 +0100

    async: V3
    * `_uri` version of `connect` and `with_connection` functions
    * Use GADT to be able to return the underlying socket to functions

--- async/s.ml
+++ async/s.ml
@@ -171,3 +171,112 @@ module type V2 = sig
     ('a -> Reader.t -> Writer.t -> unit Deferred.t) ->
     ('a, 'b) Tcp.Server.t Deferred.t
 end
+
+module type V3 = sig
+  type allowed_ciphers =
+    [ `Only of string list | `Openssl_default | `Secure ]
+  [@@deriving sexp]
+  type ssl_version [@@deriving sexp]
+  type session [@@deriving sexp_of]
+  type verify_mode [@@deriving sexp_of]
+  type ssl_opt [@@deriving sexp]
+  type ssl_conn [@@deriving sexp_of]
+
+
+  module Ssl : sig
+    module Config : sig
+      type t [@@deriving sexp_of]
+
+      val create
+        : ?version:ssl_version
+        -> ?options:ssl_opt list
+        -> ?name:string
+        -> ?hostname:string
+        -> ?allowed_ciphers:allowed_ciphers
+        -> ?ca_file:string
+        -> ?ca_path:string
+        -> ?crt_file:string
+        -> ?key_file:string
+        -> ?session:session
+        -> ?verify_modes:verify_mode list
+        -> ?verify:(ssl_conn -> bool Deferred.t)
+        -> unit
+        -> t
+    end
+  end
+
+  type _ addr =
+    | OpenSSL : Socket.Address.Inet.t * Ssl.Config.t -> Socket.Address.Inet.t addr
+    | Inet : Socket.Address.Inet.t -> Socket.Address.Inet.t addr
+    | Unix : Socket.Address.Unix.t -> Socket.Address.Unix.t addr
+  [@@deriving sexp_of]
+
+  type _ tcp_sock =
+    | Inet_sock :
+        ([`Active], Socket.Address.Inet.t) Socket.t ->
+        Socket.Address.Inet.t tcp_sock
+    | Unix_sock :
+        ([`Active], Socket.Address.Unix.t) Socket.t ->
+        Socket.Address.Unix.t tcp_sock
+
+  val resolve_uri :
+    ?options:Unix.Addr_info.getaddrinfo_option list -> Uri.t ->
+    Socket.Address.Inet.t addr Deferred.t
+
+  val connect
+    : ?interrupt:unit Deferred.t
+    -> 'a addr
+    -> ('a tcp_sock * Reader.t * Writer.t) Deferred.t
+
+  val with_connection
+    : ?interrupt:unit Deferred.t
+    -> 'a addr
+    -> ('a tcp_sock -> Reader.t -> Writer.t -> 'b Deferred.t)
+    -> 'b Deferred.t
+
+  val connect_uri :
+    ?options:Unix.Addr_info.getaddrinfo_option list ->
+    ?interrupt:unit Deferred.t
+    -> Uri.t
+    -> (Socket.Address.Inet.t tcp_sock * Reader.t * Writer.t) Deferred.t
+
+  val with_connection_uri :
+    ?options:Unix.Addr_info.getaddrinfo_option list ->
+    ?interrupt:unit Deferred.t
+    -> Uri.t
+    -> (Socket.Address.Inet.t tcp_sock -> Reader.t -> Writer.t -> 'a Deferred.t)
+    -> 'a Deferred.t
+
+  type trust_chain =
+    [ `Ca_file of string
+    | `Ca_path of string
+    | `Search_file_first_then_path of
+        [ `File of string ] *
+        [ `Path of string ]
+    ] [@@deriving sexp]
+
+  type openssl =
+    [ `OpenSSL of
+        [ `Crt_file_path of string ] *
+        [ `Key_file_path of string ]
+    ] [@@deriving sexp]
+
+  type server = [
+    | openssl
+    | `TCP
+    | `OpenSSL_with_trust_chain of
+        (openssl * trust_chain)
+  ] [@@deriving sexp]
+
+  val serve :
+    ?max_connections:int ->
+    ?backlog:int ->
+    ?buffer_age_limit:Writer.buffer_age_limit ->
+    on_handler_error:[ `Call of ([< Socket.Address.t ] as 'a) -> exn -> unit
+                     | `Ignore
+                     | `Raise ] ->
+    server ->
+    ('a, 'b) Tcp.Where_to_listen.t ->
+    ('a -> Reader.t -> Writer.t -> unit Deferred.t) ->
+    ('a, 'b) Tcp.Server.t Deferred.t
+end

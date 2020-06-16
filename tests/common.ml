44a3ce5caf7b4da56b9f84bcceb7516e53de1235
Calascibetta Romain <romain.calascibetta@gmail.com>
2020-06-16 13:50:30 +0200

    Functorize tests over LWT and ASYNC

---
+++ tests/common.ml
@@ -0,0 +1,137 @@
+module type S = sig
+  include Conduit.S
+
+  type 'a condition
+
+  val serve_with_handler :
+    handler:('flow -> unit s) ->
+    service:('cfg, 'master, 'flow) Service.service ->
+    'cfg ->
+    unit condition * unit s
+end
+
+module type CONDITION = sig
+  type 'a t
+end
+
+let ( <.> ) f g x = f (g x)
+
+module Make
+    (Scheduler : Conduit.Sigs.SCHEDULER)
+    (Condition : CONDITION)
+    (Conduit : S
+                 with type +'a s = 'a Scheduler.t
+                  and type 'a condition = 'a Condition.t
+                  and type input = Cstruct.t
+                  and type output = Cstruct.t) =
+struct
+  let return = Scheduler.return
+
+  let ( >>= ) = Scheduler.bind
+
+  let ( >>? ) x f =
+    x >>= function Ok x -> f x | Error err -> Scheduler.return (Error err)
+
+  let localhost = Domain_name.(host_exn <.> of_string_exn) "localhost"
+
+  (* Server part *)
+
+  let getline queue =
+    let exists ~predicate queue =
+      let pos = ref 0 and res = ref (-1) in
+      Ke.Rke.iter
+        (fun chr ->
+          if predicate chr then res := !pos ;
+          incr pos)
+        queue ;
+      if !res = -1 then None else Some !res in
+    let blit src src_off dst dst_off len =
+      Bigstringaf.blit_to_bytes src ~src_off dst ~dst_off ~len in
+    match exists ~predicate:(( = ) '\n') queue with
+    | Some pos ->
+        let tmp = Bytes.create pos in
+        Ke.Rke.N.keep_exn queue ~blit ~length:Bytes.length ~off:0 ~len:pos tmp ;
+        Ke.Rke.N.shift_exn queue (pos + 1) ;
+        Some (Bytes.unsafe_to_string tmp)
+    | None -> None
+
+  let getline queue flow =
+    let tmp = Cstruct.create 0x1000 in
+    let blit src src_off dst dst_off len =
+      let src = Cstruct.to_bigarray src in
+      Bigstringaf.blit src ~src_off dst ~dst_off ~len in
+    let rec go () =
+      match getline queue with
+      | Some line -> Scheduler.return (Ok (`Line line))
+      | None -> (
+          Conduit.recv flow tmp >>? function
+          | `End_of_flow -> Scheduler.return (Ok `Close)
+          | `Input len ->
+              Ke.Rke.N.push queue ~blit ~length:Cstruct.len ~off:0 ~len tmp ;
+              go ()) in
+    go ()
+
+  let pong = Cstruct.of_string "pong\n"
+
+  let ping = Cstruct.of_string "ping\n"
+
+  let transmission flow =
+    let queue = Ke.Rke.create ~capacity:0x1000 Bigarray.char in
+    let rec go () =
+      getline queue flow >>= function
+      | Ok `Close | Error _ -> Conduit.close flow
+      | Ok (`Line "ping") ->
+          Fmt.epr "[!] received ping.\n%!" ;
+          Conduit.send flow pong >>? fun _ -> go ()
+      | Ok (`Line "pong") ->
+          Fmt.epr "[!] received pong.\n%!" ;
+          Conduit.send flow ping >>? fun _ -> go ()
+      | Ok (`Line line) ->
+          Fmt.epr "[!] received %S.\n%!" line ;
+          Conduit.send flow (Cstruct.of_string (line ^ "\n")) >>? fun _ ->
+          Conduit.close flow in
+    go () >>= function
+    | Error err -> Fmt.failwith "%a" Conduit.pp_error err
+    | Ok () -> return ()
+
+  let server :
+      type cfg master flow.
+      cfg ->
+      protocol:(_, flow) Conduit.protocol ->
+      service:(cfg, master, flow) Conduit.Service.service ->
+      unit Condition.t * unit Scheduler.t =
+   fun cfg ~protocol ~service ->
+    Conduit.serve_with_handler
+      ~handler:(fun flow -> transmission (Conduit.abstract protocol flow))
+      ~service cfg
+
+  (* part *)
+
+  let client ~resolvers domain_name responses =
+    Conduit.resolve resolvers domain_name >>? fun flow ->
+    let queue = Ke.Rke.create ~capacity:0x1000 Bigarray.char in
+    let rec go = function
+      | [] -> Conduit.close flow
+      | line :: rest -> (
+          Conduit.send flow (Cstruct.of_string (line ^ "\n")) >>? fun _ ->
+          getline queue flow >>? function
+          | `Close -> Conduit.close flow
+          | `Line "pong" -> go rest
+          | `Line _ -> Conduit.close flow) in
+    go responses
+
+  let client ~resolvers filename =
+    let rec go acc ic =
+      match input_line ic with
+      | line -> go (line :: acc) ic
+      | exception End_of_file -> List.rev acc in
+    let ic = open_in filename in
+    let responses = go [] ic in
+    close_in ic ;
+    client ~resolvers localhost responses >>= function
+    | Ok () -> Scheduler.return ()
+    | Error `Closed_by_peer -> Scheduler.return ()
+    | Error (#Conduit.error as err) ->
+        Fmt.epr "client: %a.\n%!" Conduit.pp_error err ;
+        Scheduler.return ()
+end
--- tests/with_async.ml
+++ tests/with_async.ml
@@ -4,6 +4,21 @@ open Async_ssl
 
 let () = Mirage_crypto_rng_unix.initialize ()
 
+include Common.Make
+          (struct
+            type +'a t = 'a Async.Deferred.t
+
+            let bind x f = Async.Deferred.bind x ~f
+
+            let return = Async.Deferred.return
+          end)
+          (Async.Condition)
+          (struct
+            type 'a condition = 'a Async.Condition.t
+
+            include Conduit_async
+          end)
+
 let tcp_protocol, tcp_service =
   let open Conduit_async_tcp in
   (protocol, service)
@@ -16,130 +31,7 @@ let tls_protocol, tls_service =
   let open Conduit_async_tls.TCP in
   (protocol, service)
 
-let ( >>? ) x f =
-  x >>= function Ok x -> f x | Error _ as err -> Async.return err
-
 let failwith fmt = Format.kasprintf (fun err -> raise (Failure err)) fmt
-
-let getline queue =
-  let exists ~predicate queue =
-    let pos = ref 0 and res = ref (-1) in
-    Ke.Rke.iter
-      (fun chr ->
-        if predicate chr then res := !pos ;
-        incr pos)
-      queue ;
-    if !res = -1 then None else Some !res in
-  let blit src src_off dst dst_off len =
-    Bigstringaf.blit_to_bytes src ~src_off dst ~dst_off ~len in
-  match exists ~predicate:(( = ) '\n') queue with
-  | Some pos ->
-      let tmp = Bytes.create pos in
-      Ke.Rke.N.keep_exn queue ~blit ~length:Bytes.length ~off:0 ~len:pos tmp ;
-      Ke.Rke.N.shift_exn queue (pos + 1) ;
-      Some (Bytes.unsafe_to_string tmp)
-  | None -> None
-
-let getline queue flow =
-  let tmp = Cstruct.create 0x1000 in
-  let blit src src_off dst dst_off len =
-    let src = Cstruct.to_bigarray src in
-    Bigstringaf.blit src ~src_off dst ~dst_off ~len in
-  let rec go () =
-    match getline queue with
-    | Some line -> Async.return (Ok (`Line line))
-    | None -> (
-        Conduit_async.recv flow tmp >>? function
-        | `End_of_flow -> Async.return (Ok `Close)
-        | `Input len ->
-            Ke.Rke.N.push queue ~blit ~length:Cstruct.len ~off:0 ~len tmp ;
-            go ()) in
-  go ()
-
-let pong = Cstruct.of_string "pong\n"
-
-let ping = Cstruct.of_string "ping\n"
-
-let transmission ~stop flow =
-  let queue = Ke.Rke.create ~capacity:0x1000 Bigarray.Char in
-  let rec go () =
-    let finish = Condition.wait stop >>= fun () -> Async.return (Ok `Done) in
-    let getline = getline queue flow in
-    Async.Deferred.any [ finish; getline ] >>= function
-    | Ok (`Done | `Close) | Error _ -> Conduit_async.close flow
-    | Ok (`Line "ping") ->
-        Format.eprintf "[!] received ping.\n%!" ;
-        Conduit_async.send flow pong >>? fun _ -> go ()
-    | Ok (`Line "pong") ->
-        Format.eprintf "[!] received pong.\n%!" ;
-        Conduit_async.send flow ping >>? fun _ -> go ()
-    | Ok (`Line line) ->
-        Format.eprintf "[!] received %S.\n%!" line ;
-        Conduit_async.send flow (Cstruct.of_string (line ^ "\n")) >>? fun _ ->
-        Conduit_async.close flow in
-  go () >>= function
-  | Error err -> failwith "%a" Conduit_async.pp_error err
-  | Ok () -> Async.return ()
-
-let server :
-    type cfg master flow.
-    launched:unit Async.Condition.t ->
-    stop:unit Async.Condition.t ->
-    cfg ->
-    protocol:(_, flow) Conduit_async.protocol ->
-    service:(cfg, master, flow) Conduit_async.Service.service ->
-    unit Async.Deferred.t =
- fun ~launched ~stop cfg ~protocol ~service ->
-  let module Server = (val Conduit_async.Service.impl service) in
-  let main () =
-    let reword_error = R.reword_error (R.msgf "%a" Server.pp_error) in
-    Conduit_async.Service.serve cfg ~service >>? fun master ->
-    Condition.signal launched () ;
-
-    let rec go () =
-      let close = Async.Condition.wait stop >>| fun () -> Ok `Closed in
-      let accept =
-        Server.accept master >>? fun flow ->
-        Async.(Deferred.ok (return (`Flow flow))) in
-
-      Async.Deferred.any [ close; accept ] >>= function
-      | Ok (`Flow flow) ->
-          Async.don't_wait_for
-            (transmission ~stop (Conduit_async.abstract protocol flow)) ;
-          Async.Scheduler.yield () >>= go
-      | Ok `Closed -> Server.close master
-      | Error _ as err -> Server.close master >>= fun _ -> Async.return err
-    in
-    go () >>| reword_error in
-  main () >>= function
-  | Ok () -> Async.return ()
-  | Error err -> failwith "%a" Conduit_async.Service.pp_error err
-
-let client ~resolvers domain_name responses =
-  Conduit_async.resolve resolvers domain_name >>? fun flow ->
-  let queue = Ke.Rke.create ~capacity:0x1000 Bigarray.char in
-  let rec go = function
-    | [] -> Conduit_async.close flow
-    | line :: rest -> (
-        Conduit_async.send flow (Cstruct.of_string (line ^ "\n")) >>? fun _ ->
-        getline queue flow >>? function
-        | `Close -> Conduit_async.close flow
-        | `Line "pong" -> go rest
-        | `Line _ -> Conduit_async.close flow) in
-  go responses
-
-let client ~resolvers domain_name filename =
-  let rec go acc ic =
-    match Stdlib.input_line ic with
-    | line -> go (line :: acc) ic
-    | exception End_of_file -> List.rev acc in
-  let ic = Stdlib.open_in filename in
-  let responses = go [] ic in
-  Stdlib.close_in ic ;
-  client ~resolvers domain_name responses >>= function
-  | Ok () -> Async.return ()
-  | Error (#Conduit_async.error as err) ->
-      failwith "got an error: %a" Conduit_async.pp_error err
 
 let resolve_ping_pong = Conduit_async_tcp.resolv_conf ~port:5000
 
@@ -170,17 +62,19 @@ let run_with :
     string list ->
     unit =
  fun cfg ~protocol ~service clients ->
-  let launched = Condition.create () in
-  let stop = Condition.create () in
-  let server () = server ~launched ~stop cfg ~protocol ~service in
+  let stop, server = server (* ~launched ~stop *) cfg ~protocol ~service in
   let clients =
-    Condition.wait launched >>= fun () ->
-    let clients = List.map (client ~resolvers localhost) clients in
+    Async.after Core.Time.Span.(of_sec 0.5) >>= fun () ->
+    (* XXX(dinosaure): [async] tries to go further and fibers
+     * can be launched before the initialization of the server.
+     * We waiting a bit to ensure that the server is launched
+     * before clients. *)
+    let clients = List.map (client ~resolvers) clients in
     Async.Deferred.all_unit clients >>= fun () ->
     Condition.broadcast stop () ;
     Async.return () in
   Async.don't_wait_for
-    (Async.Deferred.all_unit [ server (); clients ] >>| fun () -> shutdown 0) ;
+    (Async.Deferred.all_unit [ server; clients ] >>| fun () -> shutdown 0) ;
   Core.never_returns (Scheduler.go ())
 
 let run_with_tcp clients =

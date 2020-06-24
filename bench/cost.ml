3f6438249a5832c6da2a810db56f4ff9cef16f5a
Calascibetta Romain <romain.calascibetta@gmail.com>
2020-06-24 11:01:14 +0200

    Add a proper benchmark

--- bench/cost.ml
+++ bench/cost.ml
@@ -10,8 +10,6 @@ end
 
 module Tuyau = Conduit.Make (None) (Bytes) (String)
 
-let t1 = ref 0L
-
 module Fake_protocol0 = struct
   type input = bytes
 
@@ -31,9 +29,10 @@ module Fake_protocol0 = struct
 
   let recv _ _ = Ok `End_of_flow
 
-  let send fd v =
-    t1 := tick () ;
-    let _ = Unix.write_substring fd v 0 (String.length v) in
+  let send _ _ =
+    for _ = 0 to 500 do
+      ()
+    done ;
     Ok 0
 
   let close _ = Ok ()
@@ -95,47 +94,34 @@ let fake2 = Tuyau.register ~protocol:(module Fake_protocol2)
 
 let hello_world = "Hello World!\n"
 
-let fully_abstr () =
+let fn_fully_abstr flow = Benchmark.V (fun () -> Tuyau.send flow hello_world)
+
+let fn_abstr (Tuyau.Flow (flow, (module Flow))) =
+  Benchmark.V (fun () -> Flow.send flow hello_world)
+
+let run () =
   let open Rresult in
   Tuyau.connect Unix.stderr fake0 >>= fun flow ->
-  let t0 = tick () in
-  Tuyau.send flow hello_world >>= fun _len ->
-  let t3 = Int64.sub (tick ()) !t1 in
-  let t2 = tick () in
-  Tuyau.send flow hello_world >>= fun _len ->
-  R.ok (Int64.sub !t1 t0, t3, Int64.sub !t1 t2)
+  Tuyau.send flow hello_world >>= fun _ ->
+  let samples0 = Benchmark.run (fn_fully_abstr flow) in
+  let samples1 = Benchmark.run (fn_abstr (Tuyau.flow flow)) in
 
-let abstr () =
-  let open Rresult in
-  let module Protocol = (val Tuyau.impl fake0) in
-  Tuyau.connect Unix.stderr fake0 >>= fun flow ->
-  let (Tuyau.Flow (flow, (module Flow))) = Tuyau.flow flow in
-  let t0 = tick () in
-  Flow.send flow hello_world |> R.reword_error (R.msgf "%a" Flow.pp_error)
-  >>= fun _len ->
-  let t3 = Int64.sub (tick ()) !t1 in
-  let t2 = tick () in
-  Flow.send flow hello_world |> R.reword_error (R.msgf "%a" Flow.pp_error)
-  >>= fun _len -> R.ok (Int64.sub !t1 t0, t3, Int64.sub !t1 t2)
-
-let concrete () =
-  let t0 = tick () in
-  let _ =
-    Unix.write_substring Unix.stderr hello_world 0 (String.length hello_world)
-  in
-  let t1 = tick () in
-  Ok (Int64.sub t1 t0)
+  match
+    ( Linear_algebra.ols (fun m -> m.(1)) [| (fun m -> m.(0)) |] samples0,
+      Linear_algebra.ols (fun m -> m.(1)) [| (fun m -> m.(0)) |] samples1 )
+  with
+  | Ok (estimate0, r0), Ok (estimate1, r1) ->
+      Fmt.pr "with Conduit:\t\t%fns (r²: %f).\n%!" estimate0.(0) r0 ;
+      Fmt.pr "without Conduit:\t%fns (r²: %f).\n%!" estimate1.(0) r1 ;
+      if r0 >= 0.99 && r1 >= 0.99
+      then Fmt.pr "Overhead:\t\t%fns.\n%!" (estimate0.(0) -. estimate1.(0))
+      else Fmt.epr "Bad regression coefficients!\n%!" ;
+      Ok ()
+  | Error err, _ -> Error err
+  | _, Error err -> Error err
 
 let () =
-  let _ =
-    Unix.write_substring Unix.stderr hello_world 0 (String.length hello_world)
-  in
-  let[@warning "-8"] (Ok ts) = concrete () in
-  t1 := 0L ;
-  let[@warning "-8"] (Ok (ts0, ts1, ts2)) = fully_abstr () in
-  t1 := 0L ;
-  let[@warning "-8"] (Ok (tsa, tsb, tsc)) = abstr () in
-  t1 := 0L ;
-  Fmt.pr "fully-abstr:\t%Ldns, %Ldns, %Ldns.\n%!" ts0 ts1 ts2 ;
-  Fmt.pr "abstr:\t\t%Ldns, %Ldns, %Ldns.\n%!" tsa tsb tsc ;
-  Fmt.pr "concrete:\t%Ldns.\n%!" ts
+  match run () with
+  | Ok v -> v
+  | Error (`Msg err) -> Fmt.epr "%s: %s.\n%!" Sys.argv.(0) err
+  | Error `Not_found -> assert false

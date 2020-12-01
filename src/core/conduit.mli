71c411f2191ed4c58a1ddd2bfcb486e02979e61a
Craig Ferguson <me@craigfe.io>
2020-12-01 15:06:54 +0100

    core: expand aliases exposed in _intf file
    This ensures that the OCaml toplevel is able to introspect these
    signatures directly  and prevents references to module types in
    `Conduit_intf` from escaping to the user.
    
    Fixes https://github.com/mirage/ocaml-conduit/issues/356.

--- src/core/conduit.mli
+++ src/core/conduit.mli
@@ -1,22 +1,2 @@
-module Endpoint = Endpoint
-
-type resolvers
-(** Type for resolvers map. *)
-
-val empty : resolvers
-(** [empty] is an empty {!resolvers} map. *)
-
-module type S = Conduit_intf.S
+include Conduit_intf.Conduit
 (** @inline *)
-
-module type IO = Conduit_intf.IO
-(** @inline *)
-
-module type BUFFER = Conduit_intf.BUFFER
-(** @inline *)
-
-module Make (IO : IO) (Input : BUFFER) (Output : BUFFER) :
-  S
-    with type input = Input.t
-     and type output = Output.t
-     and type +'a io = 'a IO.t

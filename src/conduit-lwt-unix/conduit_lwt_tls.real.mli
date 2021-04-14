57dcf136cb46332877d47184875dd6d5e0efbba7
Romain Calascibetta <romain.calascibetta@gmail.com>
2021-04-14 18:41:43 +0200

    Replace the alias X509_lwt.priv by Tls.Config.certchain

--- src/conduit-lwt-unix/conduit_lwt_tls.real.mli
+++ src/conduit-lwt-unix/conduit_lwt_tls.real.mli
@@ -18,7 +18,7 @@
 (** TLS/SSL connections via OCaml-TLS *)
 
 module X509 : sig
-  val private_of_pems : cert:string -> priv_key:string -> X509_lwt.priv Lwt.t
+  val private_of_pems : cert:string -> priv_key:string -> Tls.Config.certchain Lwt.t
 
   type authenticator = X509.Authenticator.t
 

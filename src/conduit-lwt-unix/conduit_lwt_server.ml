cf7c62f6c3c6edc9eb3686541fadb27800d47c65
Marcello Seri <marcello.seri@gmail.com>
2021-05-17 19:40:22 +0200

    Revert "conduit-lwt-unix.server: add a call to yield"
    This reverts commit e490592fff663d69a4984ad29275e409c1fb0e64.

--- src/conduit-lwt-unix/conduit_lwt_server.ml
+++ src/conduit-lwt-unix/conduit_lwt_server.ml
@@ -71,7 +71,7 @@ let run_handler handler v =
                   f "Uncaught exception in handler: %s" (Printexc.to_string ex)));
           Lwt.return_unit))
 
-let init ?(nconn = 10_000) ?(stop = fst (Lwt.wait ())) handler fd =
+let init ?(nconn=10_000) ?(stop = fst (Lwt.wait ())) handler fd =
   let stop = Lwt.map (fun () -> `Stop) stop in
   let log_exn = function
     | Some ex ->
@@ -100,12 +100,11 @@ let init ?(nconn = 10_000) ?(stop = fst (Lwt.wait ())) handler fd =
                 run_handler handler v;
                 Lwt.return_unit)
               connections
-            >>= Lwt_unix.yield
             >>= loop)
       (function
         | Lwt.Canceled -> Lwt.return_unit
         | ex ->
             log_exn (Some ex);
-            Lwt_unix.yield () >>= loop)
+            Lwt.return_unit >>= loop)
   in
   Lwt.finalize loop (fun () -> Lwt_unix.close fd)
